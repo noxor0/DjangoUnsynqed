60e9b73ed77e89ed9bc79d576438410af3a621b0
noxor0 <avaterq0@gmail.com>
2018-02-04 13:34:23 -0800

    few more fixes before docker commits, nothing noteworthy

--- dusite/poll/views.py
+++ dusite/poll/views.py
@@ -8,18 +8,3 @@ def poll(request):
 def poll_response(request, response):
     response_dict = models.put_vote_aws(response)
     return render(request, 'poll/poll_response.html', response_dict)
-
-# '''
-# I'm fairly sure theres a cleaner way to do this, but i dont know how to take
-# the url of the vote response, and push it into a function here.
-#
-# JK I DID IT
-# '''
-# def poll_no(request):
-#     return render(request, 'poll/poll_response.html', {'response': 'no'})
-#
-# def poll_yes(request):
-#     return render(request, 'poll/poll_response.html', {'response': 'yes'})
-#
-# def poll_maybe(request):
-#     return render(request, 'poll/poll_response.html', {'response': 'maybe'})
